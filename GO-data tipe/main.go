c3870cff4a1a96885e244c21eb83c2de35d72094
ivan7182 <muhammadivan7182@gmail.com>
2024-11-30 12:18:51 +0700

    go-data tipe

---
+++ GO-data tipe/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	isMarried := true
+	myName := "muhammad"
+	pi := 3.14
+	myAge := 24
+
+	fmt.Println("Boolean :", isMarried)
+	fmt.Println("String :", myName)
+	fmt.Println("Float :", pi)
+	fmt.Println("Integer :", myAge)
+	fmt.Printf("%v, %T\n", pi, pi)
+}
